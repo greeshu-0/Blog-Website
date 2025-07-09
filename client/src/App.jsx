785f9946178f776e722be0fef630fc0b0cc4edb4
greeshu-0 <greeshmajagarapu8878@gmail.com>
2025-07-09 16:29:29 +0530

    Created Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Layout from "./pages/admin/Layout";
 import Dashboard from "./pages/admin/Dashboard";
 import AddBlog from "./pages/admin/AddBlog";
 import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 const App = () => {
   return (
@@ -12,9 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={<Layout />}>
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
-          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="addBlog" elemen t={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
           <Route path="comments" element={<Comments />} />
         </Route>
